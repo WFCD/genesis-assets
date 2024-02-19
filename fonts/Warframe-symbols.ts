7e025856088b1b3f74fc9286a0e0956ed5608e90
Ornstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-02-19 18:08:09 -0500

    feat: add syndicate titles png, svg, and webp sigils (#2)
    Co-authored-by: Matt <7128721+TobiTenno@users.noreply.github.com>

---
+++ fonts/Warframe-symbols.ts
@@ -0,0 +1,401 @@
+export type WarframeSymbolsId =
+  | "arbitrations"
+  | "archwing"
+  | "entrati"
+  | "factions-corpus"
+  | "factions-corrupted"
+  | "factions-grineer"
+  | "factions-infested"
+  | "factions-narmer"
+  | "factions-narmer2"
+  | "factions-sentient"
+  | "fissures-1"
+  | "fissures-2"
+  | "fissures-3"
+  | "fissures-4"
+  | "fissures-5"
+  | "hexis"
+  | "kuva-kuva"
+  | "kuva-kuvaflood"
+  | "loading"
+  | "nightmare"
+  | "nightwave"
+  | "ostron"
+  | "sigils-Arbiters-of-Hexis-ArbitersLevel1"
+  | "sigils-Arbiters-of-Hexis-ArbitersLevel2"
+  | "sigils-Arbiters-of-Hexis-ArbitersLevel3"
+  | "sigils-Arbiters-of-Hexis-ArbitersLevel4"
+  | "sigils-Arbiters-of-Hexis-ArbitersLevel5"
+  | "sigils-Cephalon-Suda-CephalonSudaLevel1"
+  | "sigils-Cephalon-Suda-CephalonSudaLevel2"
+  | "sigils-Cephalon-Suda-CephalonSudaLevel3"
+  | "sigils-Cephalon-Suda-CephalonSudaLevel4"
+  | "sigils-Cephalon-Suda-CephalonSudaLevel5"
+  | "sigils-Entrati-EntratiLevel1"
+  | "sigils-Entrati-EntratiLevel2"
+  | "sigils-Entrati-EntratiLevel3"
+  | "sigils-Entrati-EntratiLevel4"
+  | "sigils-Entrati-EntratiLevel5"
+  | "sigils-Necraloid-NecraloidLevel1"
+  | "sigils-Necraloid-NecraloidLevel2"
+  | "sigils-Necraloid-NecraloidLevel3"
+  | "sigils-NegativeRank1"
+  | "sigils-NegativeRank2"
+  | "sigils-Neutral"
+  | "sigils-New-Loka-NewLokaLevel1"
+  | "sigils-New-Loka-NewLokaLevel2"
+  | "sigils-New-Loka-NewLokaLevel3"
+  | "sigils-New-Loka-NewLokaLevel4"
+  | "sigils-New-Loka-NewLokaLevel5"
+  | "sigils-Ostron-OstronLevel1"
+  | "sigils-Ostron-OstronLevel2"
+  | "sigils-Ostron-OstronLevel3"
+  | "sigils-Ostron-OstronLevel4"
+  | "sigils-Ostron-OstronLevel5"
+  | "sigils-Red-Veil-RedVeilLevel1"
+  | "sigils-Red-Veil-RedVeilLevel2"
+  | "sigils-Red-Veil-RedVeilLevel3"
+  | "sigils-Red-Veil-RedVeilLevel4"
+  | "sigils-Red-Veil-RedVeilLevel5"
+  | "sigils-Solaris-United-SolarisLevel1"
+  | "sigils-Solaris-United-SolarisLevel2"
+  | "sigils-Solaris-United-SolarisLevel3"
+  | "sigils-Solaris-United-SolarisLevel4"
+  | "sigils-Solaris-United-SolarisLevel5"
+  | "sigils-Steel-Meridian-SteelMeridianLevel1"
+  | "sigils-Steel-Meridian-SteelMeridianLevel2"
+  | "sigils-Steel-Meridian-SteelMeridianLevel3"
+  | "sigils-Steel-Meridian-SteelMeridianLevel4"
+  | "sigils-Steel-Meridian-SteelMeridianLevel5"
+  | "sigils-The-Holdfasts-HoldfastsLevel1"
+  | "sigils-The-Holdfasts-HoldfastsLevel2"
+  | "sigils-The-Holdfasts-HoldfastsLevel3"
+  | "sigils-The-Holdfasts-HoldfastsLevel4"
+  | "sigils-The-Holdfasts-HoldfastsLevel5"
+  | "sigils-The-Perrin-Sequence-PerrinLevel1"
+  | "sigils-The-Perrin-Sequence-PerrinLevel2"
+  | "sigils-The-Perrin-Sequence-PerrinLevel3"
+  | "sigils-The-Perrin-Sequence-PerrinLevel4"
+  | "sigils-The-Perrin-Sequence-PerrinLevel5"
+  | "sigils-The-Quills-QuillsLevel1"
+  | "sigils-The-Quills-QuillsLevel2"
+  | "sigils-The-Quills-QuillsLevel3"
+  | "sigils-The-Quills-QuillsLevel4"
+  | "sigils-The-Quills-QuillsLevel5"
+  | "sigils-VentKids-VentKidsLevel1"
+  | "sigils-VentKids-VentKidsLevel2"
+  | "sigils-VentKids-VentKidsLevel3"
+  | "sigils-VentKids-VentKidsLevel4"
+  | "sigils-VentKids-VentKidsLevel5"
+  | "sigils-Vox-Solaris-VoxLevel1"
+  | "sigils-Vox-Solaris-VoxLevel2"
+  | "sigils-Vox-Solaris-VoxLevel3"
+  | "sigils-Vox-Solaris-VoxLevel4"
+  | "sigils-Vox-Solaris-VoxLevel5"
+  | "simaris"
+  | "solaris"
+  | "sp-logo"
+  | "standing"
+  | "wfcd";
+
+export type WarframeSymbolsKey =
+  | "Arbitrations"
+  | "Archwing"
+  | "Entrati"
+  | "FactionsCorpus"
+  | "FactionsCorrupted"
+  | "FactionsGrineer"
+  | "FactionsInfested"
+  | "FactionsNarmer"
+  | "FactionsNarmer2"
+  | "FactionsSentient"
+  | "Fissures_1"
+  | "Fissures_2"
+  | "Fissures_3"
+  | "Fissures_4"
+  | "Fissures_5"
+  | "Hexis"
+  | "KuvaKuva"
+  | "KuvaKuvaflood"
+  | "Loading"
+  | "Nightmare"
+  | "Nightwave"
+  | "Ostron"
+  | "SigilsArbitersOfHexisArbitersLevel1"
+  | "SigilsArbitersOfHexisArbitersLevel2"
+  | "SigilsArbitersOfHexisArbitersLevel3"
+  | "SigilsArbitersOfHexisArbitersLevel4"
+  | "SigilsArbitersOfHexisArbitersLevel5"
+  | "SigilsCephalonSudaCephalonSudaLevel1"
+  | "SigilsCephalonSudaCephalonSudaLevel2"
+  | "SigilsCephalonSudaCephalonSudaLevel3"
+  | "SigilsCephalonSudaCephalonSudaLevel4"
+  | "SigilsCephalonSudaCephalonSudaLevel5"
+  | "SigilsEntratiEntratiLevel1"
+  | "SigilsEntratiEntratiLevel2"
+  | "SigilsEntratiEntratiLevel3"
+  | "SigilsEntratiEntratiLevel4"
+  | "SigilsEntratiEntratiLevel5"
+  | "SigilsNecraloidNecraloidLevel1"
+  | "SigilsNecraloidNecraloidLevel2"
+  | "SigilsNecraloidNecraloidLevel3"
+  | "SigilsNegativeRank1"
+  | "SigilsNegativeRank2"
+  | "SigilsNeutral"
+  | "SigilsNewLokaNewLokaLevel1"
+  | "SigilsNewLokaNewLokaLevel2"
+  | "SigilsNewLokaNewLokaLevel3"
+  | "SigilsNewLokaNewLokaLevel4"
+  | "SigilsNewLokaNewLokaLevel5"
+  | "SigilsOstronOstronLevel1"
+  | "SigilsOstronOstronLevel2"
+  | "SigilsOstronOstronLevel3"
+  | "SigilsOstronOstronLevel4"
+  | "SigilsOstronOstronLevel5"
+  | "SigilsRedVeilRedVeilLevel1"
+  | "SigilsRedVeilRedVeilLevel2"
+  | "SigilsRedVeilRedVeilLevel3"
+  | "SigilsRedVeilRedVeilLevel4"
+  | "SigilsRedVeilRedVeilLevel5"
+  | "SigilsSolarisUnitedSolarisLevel1"
+  | "SigilsSolarisUnitedSolarisLevel2"
+  | "SigilsSolarisUnitedSolarisLevel3"
+  | "SigilsSolarisUnitedSolarisLevel4"
+  | "SigilsSolarisUnitedSolarisLevel5"
+  | "SigilsSteelMeridianSteelMeridianLevel1"
+  | "SigilsSteelMeridianSteelMeridianLevel2"
+  | "SigilsSteelMeridianSteelMeridianLevel3"
+  | "SigilsSteelMeridianSteelMeridianLevel4"
+  | "SigilsSteelMeridianSteelMeridianLevel5"
+  | "SigilsTheHoldfastsHoldfastsLevel1"
+  | "SigilsTheHoldfastsHoldfastsLevel2"
+  | "SigilsTheHoldfastsHoldfastsLevel3"
+  | "SigilsTheHoldfastsHoldfastsLevel4"
+  | "SigilsTheHoldfastsHoldfastsLevel5"
+  | "SigilsThePerrinSequencePerrinLevel1"
+  | "SigilsThePerrinSequencePerrinLevel2"
+  | "SigilsThePerrinSequencePerrinLevel3"
+  | "SigilsThePerrinSequencePerrinLevel4"
+  | "SigilsThePerrinSequencePerrinLevel5"
+  | "SigilsTheQuillsQuillsLevel1"
+  | "SigilsTheQuillsQuillsLevel2"
+  | "SigilsTheQuillsQuillsLevel3"
+  | "SigilsTheQuillsQuillsLevel4"
+  | "SigilsTheQuillsQuillsLevel5"
+  | "SigilsVentKidsVentKidsLevel1"
+  | "SigilsVentKidsVentKidsLevel2"
+  | "SigilsVentKidsVentKidsLevel3"
+  | "SigilsVentKidsVentKidsLevel4"
+  | "SigilsVentKidsVentKidsLevel5"
+  | "SigilsVoxSolarisVoxLevel1"
+  | "SigilsVoxSolarisVoxLevel2"
+  | "SigilsVoxSolarisVoxLevel3"
+  | "SigilsVoxSolarisVoxLevel4"
+  | "SigilsVoxSolarisVoxLevel5"
+  | "Simaris"
+  | "Solaris"
+  | "SpLogo"
+  | "Standing"
+  | "Wfcd";
+
+export enum WarframeSymbols {
+  Arbitrations = "arbitrations",
+  Archwing = "archwing",
+  Entrati = "entrati",
+  FactionsCorpus = "factions-corpus",
+  FactionsCorrupted = "factions-corrupted",
+  FactionsGrineer = "factions-grineer",
+  FactionsInfested = "factions-infested",
+  FactionsNarmer = "factions-narmer",
+  FactionsNarmer2 = "factions-narmer2",
+  FactionsSentient = "factions-sentient",
+  Fissures_1 = "fissures-1",
+  Fissures_2 = "fissures-2",
+  Fissures_3 = "fissures-3",
+  Fissures_4 = "fissures-4",
+  Fissures_5 = "fissures-5",
+  Hexis = "hexis",
+  KuvaKuva = "kuva-kuva",
+  KuvaKuvaflood = "kuva-kuvaflood",
+  Loading = "loading",
+  Nightmare = "nightmare",
+  Nightwave = "nightwave",
+  Ostron = "ostron",
+  SigilsArbitersOfHexisArbitersLevel1 = "sigils-Arbiters-of-Hexis-ArbitersLevel1",
+  SigilsArbitersOfHexisArbitersLevel2 = "sigils-Arbiters-of-Hexis-ArbitersLevel2",
+  SigilsArbitersOfHexisArbitersLevel3 = "sigils-Arbiters-of-Hexis-ArbitersLevel3",
+  SigilsArbitersOfHexisArbitersLevel4 = "sigils-Arbiters-of-Hexis-ArbitersLevel4",
+  SigilsArbitersOfHexisArbitersLevel5 = "sigils-Arbiters-of-Hexis-ArbitersLevel5",
+  SigilsCephalonSudaCephalonSudaLevel1 = "sigils-Cephalon-Suda-CephalonSudaLevel1",
+  SigilsCephalonSudaCephalonSudaLevel2 = "sigils-Cephalon-Suda-CephalonSudaLevel2",
+  SigilsCephalonSudaCephalonSudaLevel3 = "sigils-Cephalon-Suda-CephalonSudaLevel3",
+  SigilsCephalonSudaCephalonSudaLevel4 = "sigils-Cephalon-Suda-CephalonSudaLevel4",
+  SigilsCephalonSudaCephalonSudaLevel5 = "sigils-Cephalon-Suda-CephalonSudaLevel5",
+  SigilsEntratiEntratiLevel1 = "sigils-Entrati-EntratiLevel1",
+  SigilsEntratiEntratiLevel2 = "sigils-Entrati-EntratiLevel2",
+  SigilsEntratiEntratiLevel3 = "sigils-Entrati-EntratiLevel3",
+  SigilsEntratiEntratiLevel4 = "sigils-Entrati-EntratiLevel4",
+  SigilsEntratiEntratiLevel5 = "sigils-Entrati-EntratiLevel5",
+  SigilsNecraloidNecraloidLevel1 = "sigils-Necraloid-NecraloidLevel1",
+  SigilsNecraloidNecraloidLevel2 = "sigils-Necraloid-NecraloidLevel2",
+  SigilsNecraloidNecraloidLevel3 = "sigils-Necraloid-NecraloidLevel3",
+  SigilsNegativeRank1 = "sigils-NegativeRank1",
+  SigilsNegativeRank2 = "sigils-NegativeRank2",
+  SigilsNeutral = "sigils-Neutral",
+  SigilsNewLokaNewLokaLevel1 = "sigils-New-Loka-NewLokaLevel1",
+  SigilsNewLokaNewLokaLevel2 = "sigils-New-Loka-NewLokaLevel2",
+  SigilsNewLokaNewLokaLevel3 = "sigils-New-Loka-NewLokaLevel3",
+  SigilsNewLokaNewLokaLevel4 = "sigils-New-Loka-NewLokaLevel4",
+  SigilsNewLokaNewLokaLevel5 = "sigils-New-Loka-NewLokaLevel5",
+  SigilsOstronOstronLevel1 = "sigils-Ostron-OstronLevel1",
+  SigilsOstronOstronLevel2 = "sigils-Ostron-OstronLevel2",
+  SigilsOstronOstronLevel3 = "sigils-Ostron-OstronLevel3",
+  SigilsOstronOstronLevel4 = "sigils-Ostron-OstronLevel4",
+  SigilsOstronOstronLevel5 = "sigils-Ostron-OstronLevel5",
+  SigilsRedVeilRedVeilLevel1 = "sigils-Red-Veil-RedVeilLevel1",
+  SigilsRedVeilRedVeilLevel2 = "sigils-Red-Veil-RedVeilLevel2",
+  SigilsRedVeilRedVeilLevel3 = "sigils-Red-Veil-RedVeilLevel3",
+  SigilsRedVeilRedVeilLevel4 = "sigils-Red-Veil-RedVeilLevel4",
+  SigilsRedVeilRedVeilLevel5 = "sigils-Red-Veil-RedVeilLevel5",
+  SigilsSolarisUnitedSolarisLevel1 = "sigils-Solaris-United-SolarisLevel1",
+  SigilsSolarisUnitedSolarisLevel2 = "sigils-Solaris-United-SolarisLevel2",
+  SigilsSolarisUnitedSolarisLevel3 = "sigils-Solaris-United-SolarisLevel3",
+  SigilsSolarisUnitedSolarisLevel4 = "sigils-Solaris-United-SolarisLevel4",
+  SigilsSolarisUnitedSolarisLevel5 = "sigils-Solaris-United-SolarisLevel5",
+  SigilsSteelMeridianSteelMeridianLevel1 = "sigils-Steel-Meridian-SteelMeridianLevel1",
+  SigilsSteelMeridianSteelMeridianLevel2 = "sigils-Steel-Meridian-SteelMeridianLevel2",
+  SigilsSteelMeridianSteelMeridianLevel3 = "sigils-Steel-Meridian-SteelMeridianLevel3",
+  SigilsSteelMeridianSteelMeridianLevel4 = "sigils-Steel-Meridian-SteelMeridianLevel4",
+  SigilsSteelMeridianSteelMeridianLevel5 = "sigils-Steel-Meridian-SteelMeridianLevel5",
+  SigilsTheHoldfastsHoldfastsLevel1 = "sigils-The-Holdfasts-HoldfastsLevel1",
+  SigilsTheHoldfastsHoldfastsLevel2 = "sigils-The-Holdfasts-HoldfastsLevel2",
+  SigilsTheHoldfastsHoldfastsLevel3 = "sigils-The-Holdfasts-HoldfastsLevel3",
+  SigilsTheHoldfastsHoldfastsLevel4 = "sigils-The-Holdfasts-HoldfastsLevel4",
+  SigilsTheHoldfastsHoldfastsLevel5 = "sigils-The-Holdfasts-HoldfastsLevel5",
+  SigilsThePerrinSequencePerrinLevel1 = "sigils-The-Perrin-Sequence-PerrinLevel1",
+  SigilsThePerrinSequencePerrinLevel2 = "sigils-The-Perrin-Sequence-PerrinLevel2",
+  SigilsThePerrinSequencePerrinLevel3 = "sigils-The-Perrin-Sequence-PerrinLevel3",
+  SigilsThePerrinSequencePerrinLevel4 = "sigils-The-Perrin-Sequence-PerrinLevel4",
+  SigilsThePerrinSequencePerrinLevel5 = "sigils-The-Perrin-Sequence-PerrinLevel5",
+  SigilsTheQuillsQuillsLevel1 = "sigils-The-Quills-QuillsLevel1",
+  SigilsTheQuillsQuillsLevel2 = "sigils-The-Quills-QuillsLevel2",
+  SigilsTheQuillsQuillsLevel3 = "sigils-The-Quills-QuillsLevel3",
+  SigilsTheQuillsQuillsLevel4 = "sigils-The-Quills-QuillsLevel4",
+  SigilsTheQuillsQuillsLevel5 = "sigils-The-Quills-QuillsLevel5",
+  SigilsVentKidsVentKidsLevel1 = "sigils-VentKids-VentKidsLevel1",
+  SigilsVentKidsVentKidsLevel2 = "sigils-VentKids-VentKidsLevel2",
+  SigilsVentKidsVentKidsLevel3 = "sigils-VentKids-VentKidsLevel3",
+  SigilsVentKidsVentKidsLevel4 = "sigils-VentKids-VentKidsLevel4",
+  SigilsVentKidsVentKidsLevel5 = "sigils-VentKids-VentKidsLevel5",
+  SigilsVoxSolarisVoxLevel1 = "sigils-Vox-Solaris-VoxLevel1",
+  SigilsVoxSolarisVoxLevel2 = "sigils-Vox-Solaris-VoxLevel2",
+  SigilsVoxSolarisVoxLevel3 = "sigils-Vox-Solaris-VoxLevel3",
+  SigilsVoxSolarisVoxLevel4 = "sigils-Vox-Solaris-VoxLevel4",
+  SigilsVoxSolarisVoxLevel5 = "sigils-Vox-Solaris-VoxLevel5",
+  Simaris = "simaris",
+  Solaris = "solaris",
+  SpLogo = "sp-logo",
+  Standing = "standing",
+  Wfcd = "wfcd",
+}
+
+export const WARFRAME_SYMBOLS_CODEPOINTS: { [key in WarframeSymbols]: string } = {
+  [WarframeSymbols.Arbitrations]: "61697",
+  [WarframeSymbols.Archwing]: "61698",
+  [WarframeSymbols.Entrati]: "61699",
+  [WarframeSymbols.FactionsCorpus]: "61700",
+  [WarframeSymbols.FactionsCorrupted]: "61701",
+  [WarframeSymbols.FactionsGrineer]: "61702",
+  [WarframeSymbols.FactionsInfested]: "61703",
+  [WarframeSymbols.FactionsNarmer]: "61704",
+  [WarframeSymbols.FactionsNarmer2]: "61705",
+  [WarframeSymbols.FactionsSentient]: "61706",
+  [WarframeSymbols.Fissures_1]: "61707",
+  [WarframeSymbols.Fissures_2]: "61708",
+  [WarframeSymbols.Fissures_3]: "61709",
+  [WarframeSymbols.Fissures_4]: "61710",
+  [WarframeSymbols.Fissures_5]: "61711",
+  [WarframeSymbols.Hexis]: "61712",
+  [WarframeSymbols.KuvaKuva]: "61713",
+  [WarframeSymbols.KuvaKuvaflood]: "61714",
+  [WarframeSymbols.Loading]: "61715",
+  [WarframeSymbols.Nightmare]: "61716",
+  [WarframeSymbols.Nightwave]: "61717",
+  [WarframeSymbols.Ostron]: "61718",
+  [WarframeSymbols.SigilsArbitersOfHexisArbitersLevel1]: "61719",
+  [WarframeSymbols.SigilsArbitersOfHexisArbitersLevel2]: "61720",
+  [WarframeSymbols.SigilsArbitersOfHexisArbitersLevel3]: "61721",
+  [WarframeSymbols.SigilsArbitersOfHexisArbitersLevel4]: "61722",
+  [WarframeSymbols.SigilsArbitersOfHexisArbitersLevel5]: "61723",
+  [WarframeSymbols.SigilsCephalonSudaCephalonSudaLevel1]: "61724",
+  [WarframeSymbols.SigilsCephalonSudaCephalonSudaLevel2]: "61725",
+  [WarframeSymbols.SigilsCephalonSudaCephalonSudaLevel3]: "61726",
+  [WarframeSymbols.SigilsCephalonSudaCephalonSudaLevel4]: "61727",
+  [WarframeSymbols.SigilsCephalonSudaCephalonSudaLevel5]: "61728",
+  [WarframeSymbols.SigilsEntratiEntratiLevel1]: "61729",
+  [WarframeSymbols.SigilsEntratiEntratiLevel2]: "61730",
+  [WarframeSymbols.SigilsEntratiEntratiLevel3]: "61731",
+  [WarframeSymbols.SigilsEntratiEntratiLevel4]: "61732",
+  [WarframeSymbols.SigilsEntratiEntratiLevel5]: "61733",
+  [WarframeSymbols.SigilsNecraloidNecraloidLevel1]: "61734",
+  [WarframeSymbols.SigilsNecraloidNecraloidLevel2]: "61735",
+  [WarframeSymbols.SigilsNecraloidNecraloidLevel3]: "61736",
+  [WarframeSymbols.SigilsNegativeRank1]: "61737",
+  [WarframeSymbols.SigilsNegativeRank2]: "61738",
+  [WarframeSymbols.SigilsNeutral]: "61739",
+  [WarframeSymbols.SigilsNewLokaNewLokaLevel1]: "61740",
+  [WarframeSymbols.SigilsNewLokaNewLokaLevel2]: "61741",
+  [WarframeSymbols.SigilsNewLokaNewLokaLevel3]: "61742",
+  [WarframeSymbols.SigilsNewLokaNewLokaLevel4]: "61743",
+  [WarframeSymbols.SigilsNewLokaNewLokaLevel5]: "61744",
+  [WarframeSymbols.SigilsOstronOstronLevel1]: "61745",
+  [WarframeSymbols.SigilsOstronOstronLevel2]: "61746",
+  [WarframeSymbols.SigilsOstronOstronLevel3]: "61747",
+  [WarframeSymbols.SigilsOstronOstronLevel4]: "61748",
+  [WarframeSymbols.SigilsOstronOstronLevel5]: "61749",
+  [WarframeSymbols.SigilsRedVeilRedVeilLevel1]: "61750",
+  [WarframeSymbols.SigilsRedVeilRedVeilLevel2]: "61751",
+  [WarframeSymbols.SigilsRedVeilRedVeilLevel3]: "61752",
+  [WarframeSymbols.SigilsRedVeilRedVeilLevel4]: "61753",
+  [WarframeSymbols.SigilsRedVeilRedVeilLevel5]: "61754",
+  [WarframeSymbols.SigilsSolarisUnitedSolarisLevel1]: "61755",
+  [WarframeSymbols.SigilsSolarisUnitedSolarisLevel2]: "61756",
+  [WarframeSymbols.SigilsSolarisUnitedSolarisLevel3]: "61757",
+  [WarframeSymbols.SigilsSolarisUnitedSolarisLevel4]: "61758",
+  [WarframeSymbols.SigilsSolarisUnitedSolarisLevel5]: "61759",
+  [WarframeSymbols.SigilsSteelMeridianSteelMeridianLevel1]: "61760",
+  [WarframeSymbols.SigilsSteelMeridianSteelMeridianLevel2]: "61761",
+  [WarframeSymbols.SigilsSteelMeridianSteelMeridianLevel3]: "61762",
+  [WarframeSymbols.SigilsSteelMeridianSteelMeridianLevel4]: "61763",
+  [WarframeSymbols.SigilsSteelMeridianSteelMeridianLevel5]: "61764",
+  [WarframeSymbols.SigilsTheHoldfastsHoldfastsLevel1]: "61765",
+  [WarframeSymbols.SigilsTheHoldfastsHoldfastsLevel2]: "61766",
+  [WarframeSymbols.SigilsTheHoldfastsHoldfastsLevel3]: "61767",
+  [WarframeSymbols.SigilsTheHoldfastsHoldfastsLevel4]: "61768",
+  [WarframeSymbols.SigilsTheHoldfastsHoldfastsLevel5]: "61769",
+  [WarframeSymbols.SigilsThePerrinSequencePerrinLevel1]: "61770",
+  [WarframeSymbols.SigilsThePerrinSequencePerrinLevel2]: "61771",
+  [WarframeSymbols.SigilsThePerrinSequencePerrinLevel3]: "61772",
+  [WarframeSymbols.SigilsThePerrinSequencePerrinLevel4]: "61773",
+  [WarframeSymbols.SigilsThePerrinSequencePerrinLevel5]: "61774",
+  [WarframeSymbols.SigilsTheQuillsQuillsLevel1]: "61775",
+  [WarframeSymbols.SigilsTheQuillsQuillsLevel2]: "61776",
+  [WarframeSymbols.SigilsTheQuillsQuillsLevel3]: "61777",
+  [WarframeSymbols.SigilsTheQuillsQuillsLevel4]: "61778",
+  [WarframeSymbols.SigilsTheQuillsQuillsLevel5]: "61779",
+  [WarframeSymbols.SigilsVentKidsVentKidsLevel1]: "61780",
+  [WarframeSymbols.SigilsVentKidsVentKidsLevel2]: "61781",
+  [WarframeSymbols.SigilsVentKidsVentKidsLevel3]: "61782",
+  [WarframeSymbols.SigilsVentKidsVentKidsLevel4]: "61783",
+  [WarframeSymbols.SigilsVentKidsVentKidsLevel5]: "61784",
+  [WarframeSymbols.SigilsVoxSolarisVoxLevel1]: "61785",
+  [WarframeSymbols.SigilsVoxSolarisVoxLevel2]: "61786",
+  [WarframeSymbols.SigilsVoxSolarisVoxLevel3]: "61787",
+  [WarframeSymbols.SigilsVoxSolarisVoxLevel4]: "61788",
+  [WarframeSymbols.SigilsVoxSolarisVoxLevel5]: "61789",
+  [WarframeSymbols.Simaris]: "61790",
+  [WarframeSymbols.Solaris]: "61791",
+  [WarframeSymbols.SpLogo]: "61792",
+  [WarframeSymbols.Standing]: "61793",
+  [WarframeSymbols.Wfcd]: "61794",
+};
