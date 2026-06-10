f3ceb75f0c6b4f9afaab6cc6fa1360d97a53e00f
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2026-06-10 16:08:13 -0400

    chore: update script to also update font icons

--- scripts/batch-png-to-svg.sh
+++ scripts/batch-png-to-svg.sh
@@ -2,32 +2,34 @@
 
 # Right now this method only works with DE's simple icon PNGs so I wouldn't try this with
 # anything more complex then an icon. Even then the original would have to be something >= 256x256
-# otherwise you could have misalinged lines and basically blobs
+# otherwise you could have misalinged lines and blobs
+
+indir=$1
+outdir=$2
+
+mkdir temp
+mkdir -p $outdir
 
 # Loop through PNG files
-for file in *.png; do
+for file in $indir/*.png; do
   # Extract filename without extension
-  basename=$(echo "$file" | sed 's/\.\w*$//')
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
 
-  # Convert PNG to black PNG with transparency
-  # We need to convert the image from black to white so potrace can detect
-  # and trace properly
-  convert "$file" \
-    -channel RGB \
-    -fuzz 99% \
-    -fill black \
-    -opaque white \
+  # Potrace needs black lines to trace properly, this sttep sets the transparent background to black then inverts the color
+  magick "$file" \
+    -background black \
+    -alpha remove \
+    -negate \
     -set filename:base "%[basename]" \
-    "%[filename:base].png"
-
-  # Convert black PNG to PNM
-  pngtopnm -mix "$basename.png" > "$basename.pnm"
+    "temp/%[filename:base].bmp"
 
   # Convert PNM to black SVG with Potrace
-  potrace "$basename.pnm" --svg --color "#e5e5e5" --progress > "$basename.svg"
-
-  # Clean up intermediate PNM file
-  rm "$basename.pnm"
+  potrace "temp/$filename.bmp" --svg --color "#f2f2f2" --progress -o "$outdir/$filename.svg"
 done
 
-echo "Finished converting PNG icons to black SVGs!"
+rm -rf temp
+echo "Finished batch conversion"
+
+echo "Updating Warframe-Symbols"
+fantasticon --config fonts/.fantasticonrc.js
